f804f42bba2ffd155fb0b0feeb747242d159db20
chithaluri chandrashekar <chandrashekar976@gmail.com>
2023-05-31 21:07:37 +0530

    first commit

---
+++ src/main/java/com/gitproject4/project4/App.java
@@ -0,0 +1,13 @@
+package com.gitproject4.project4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
